9b2aff1be08378ba2a4818eb1268fc7851f2bb6c
Ben Pickles <spideryoung@gmail.com>
2018-02-13 18:11:54 +0000

    Add missing dependency.

--- lib/aesthetic/rspec_helpers.rb
+++ lib/aesthetic/rspec_helpers.rb
@@ -1,11 +1,15 @@
 require 'aesthetic'
+require 'active_support/inflector/methods'
 
 module Aesthetic
   module RSpecHelpers
     PREFIX = /^r_spec\/example_groups\//
 
     def screenshot
-      directory = self.class.to_s.underscore.sub(PREFIX, '')
+      directory = ActiveSupport::Inflector.underscore(
+        self.class.to_s
+      ).sub(PREFIX, '')
+
       Aesthetic.screenshot(page, directory)
     end
   end
